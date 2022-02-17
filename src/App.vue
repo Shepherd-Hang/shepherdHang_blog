189ef798f799d469254f7a5752fb0c4cf95157e5
shepherd <1808131636@qq.com>
2022-02-17 17:36:48 +0800

    完成头部菜单栏header-nav

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,111 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <header>
+    <nav class="navbar">
+      <div class="logo-box">
+        <a class="logo" target="_blank">
+          <video class="logo-vidio-bg" preload="preload" muted="true" autoplay autobuffer="true" loop="loop" poster>
+            <source type="video/webm" src="https://gamevideo.wmupd.com/dota2media/media/coverlogo180517.webm">
+            <source type="video/mp4" src="https://gamevideo.wmupd.com/dota2media/media/coverlogo180517.mp4">
+          </video>
+        </a>
+      </div>
+      <ul class="top-nav">
+        <li>
+            <router-link to="/">Home</router-link>
+        </li>
+        <li>
+            <router-link to="">About</router-link>
+        </li>
+        <li>
+            <router-link to="">Speech</router-link>
+        </li>
+        <li>
+            <router-link to="">Contact</router-link>
+        </li>
+        <li>
+            <router-link to="">About</router-link>
+        </li>
+      </ul>
+    </nav>
+
+  </header>
+
+  <router-view />
 </template>
 
+<script>
+export default {}
+</script>
+
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
+header {
+  background: url('./assets/header.png') center top no-repeat;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  left: 0;
+  height: 90px;
+  z-index: 9999;
+  min-width: 1220px;
   a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+      color: #ffffff;
+  }
+  .navbar {
+    position: relative;
+    width: 1220px;
+    margin: 0 auto;
+    height: 90px;
+    padding-left: 213px;
+    overflow: hidden;
+    .logo-box {
+      position: absolute;
+      top: -3px;
+      left: 14px;
+      width: 189px;
+      height: 83px;
+      overflow: hidden;
+      .logo {
+        display: block;
+        height: 100%;
+      }
+    }
+    .top-nav {
+      overflow: hidden;
+      li:first-child {
+        margin-left: 0;
+      }
+      li {
+        width: 128px;
+        float: left;
+        text-align: center;
+        margin-left: 35px;
+        padding-top: 23px;
+        padding-bottom: 2px;
+        a {
+          display: block;
+          font-size: 16px;
+          color: #dedede;
+          letter-spacing: 1px;
+          line-height: 100%;
+          position: relative;
+        }
+      }
     }
   }
 }
+
+body {
+   min-width: 1300px;
+   max-width: 1920px;
+   margin: 0 auto;
+   padding: 0;
+   font-size: 12px;
+   font-family: 'Microsoft YaHei', Arial, Helvetica, sans-serif;
+   -ms-text-size-adjust: 100%;
+   -webkit-text-size-adjust: 100%;
+   background-color: #181818 !important;
+   text-size-adjust: 100% !important;
+}
+
 </style>
